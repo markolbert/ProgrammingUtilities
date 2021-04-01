d98f6ac3507e0919618820590d7fc9a845789783
Mark Olbert <mark@arcabama.com>
2021-04-01 11:01:44 -0700

    Added OS utilities for EF

---
+++ EFCoreUtilities/OSCaseSensitivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace J4JSoftware.EFCoreUtilities
+{
+    public static class OsUtilities
+    {
+        public static bool IsFileSystemCaseSensitive() => Environment.OSVersion.Platform switch
+        {
+            PlatformID.Win32NT => false,
+            PlatformID.MacOSX => true,
+            PlatformID.Unix => true,
+            PlatformID.Win32S => false,
+            PlatformID.Win32Windows => false,
+            PlatformID.WinCE => false,
+            PlatformID.Xbox => false,
+            _ => false
+        };
+
+        public static string SqliteCollation( this SqliteCollationType type ) =>
+            type switch
+            {
+                SqliteCollationType.IgnoreTrailingWhitespace => "RTRIM",
+                SqliteCollationType.CaseInsensitiveAtoZ => "NOCASE",
+                SqliteCollationType.CaseSensitive => "BINARY",
+                _ => throw new InvalidEnumArgumentException( $"Unhandled {typeof(SqliteCollationType)} value '{type}'" )
+            };
+
+        public static StringComparison FileSystemComparison =>
+            IsFileSystemCaseSensitive() ? StringComparison.Ordinal : StringComparison.OrdinalIgnoreCase;
+    }
+}
--- EFCoreUtilities/SqliteCollationType.cs
+++ EFCoreUtilities/SqliteCollationType.cs
@@ -17,27 +17,12 @@
 
 #endregion
 
-using System.Linq;
-using System.Reflection;
-using Microsoft.EntityFrameworkCore;
-
 namespace J4JSoftware.EFCoreUtilities
 {
-    public static class EntityConfigurationExtensions
+    public enum SqliteCollationType
     {
-        public static void ConfigureEntities( this ModelBuilder modelBuilder, Assembly assemblyToScan )
-        {
-            if( assemblyToScan == null )
-                return;
-
-            // scan current assembly for types decorated with EntityConfigurationAttribute
-            foreach( var entityType in assemblyToScan.DefinedTypes
-                .Where( t => t.GetCustomAttribute<EntityConfigurationAttribute>() != null ) )
-            {
-                var attr = entityType.GetCustomAttribute<EntityConfigurationAttribute>();
-
-                attr?.GetConfigurator().Configure( modelBuilder );
-            }
-        }
+        IgnoreTrailingWhitespace,
+        CaseInsensitiveAtoZ,
+        CaseSensitive
     }
 }
