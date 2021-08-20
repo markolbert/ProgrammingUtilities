b7e3e2fe839aa1cd7e408e916eb66a5044da1eb2
Mark Olbert <mark@arcabama.com>
2021-08-20 08:50:28 -0700

    Moved type testing to separate project to eliminate implicit Autofac dependency

---
+++ TypeUtilities/TUExtensions.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using J4JSoftware.Logging;
+
+namespace J4JSoftware.DependencyInjection
+{
+    public static class TUExtensions
+    {
+        public static IEnumerable<Type> MeetRequirements<T>(
+            this IEnumerable<Type> types,
+            params ITypeTester[] tests
+        )
+            where T : class =>
+            types.Where(t => typeof(T).IsAssignableFrom(t)
+                             && tests.All(x => x.MeetsRequirements(t)));
+
+        public static IEnumerable<Type> MeetRequirements<T>(
+            this IEnumerable<Type> types,
+            IEnumerable<ITypeTester> tests
+        )
+            where T: class =>
+            types.Where( t => typeof( T ).IsAssignableFrom( t )
+                              && tests.All( x => x.MeetsRequirements( t ) ) );
+
+        public static IEnumerable<Type> MeetRequirements<T>(
+            this IEnumerable<Type> types,
+            params PredefinedTypeTests[] predefinedTests )
+            where T: class
+            => types.MeetRequirements<T>( predefinedTests.ToTypeTesters<T>() );
+
+        public static IEnumerable<Type> MeetRequirements<T>(
+            this IEnumerable<Type> types,
+            IEnumerable<PredefinedTypeTests> predefinedTests)
+            where T : class
+            => types.MeetRequirements<T>(predefinedTests.ToTypeTesters<T>());
+
+        private static List<ITypeTester> ToTypeTesters<T>( this IEnumerable<PredefinedTypeTests> predefinedTests )
+        where T: class
+        {
+            var retVal = new List<ITypeTester>();
+
+            foreach (var test in predefinedTests.Distinct())
+            {
+                switch (test)
+                {
+                    case PredefinedTypeTests.ParameterlessConstructor:
+                        retVal.Add(new ConstructorTester<T>());
+                        break;
+
+                    case PredefinedTypeTests.OnlyJ4JLoggerRequired:
+                        retVal.Add(new ConstructorTester<T>(typeof(IJ4JLogger)));
+                        break;
+
+                    case PredefinedTypeTests.OnlyJ4JLoggerFactoryRequired:
+                        retVal.Add(new ConstructorTester<T>(typeof(IJ4JLoggerFactory)));
+                        break;
+
+                    case PredefinedTypeTests.NonAbstract:
+                        retVal.Add(TypeTester.NonAbstract);
+                        break;
+
+                    default:
+                        throw new InvalidEnumArgumentException(
+                            $"Unsupported {nameof(PredefinedTypeTests)} value '{test}'");
+                }
+            }
+
+            return retVal;
+        }
+    }
+}
