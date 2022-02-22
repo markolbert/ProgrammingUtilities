eb14b00550a8b6e65048894f7b3a0d096f8b46a7
Mark Olbert <mark@arcabama.com>
2022-02-22 10:22:07 -0800

    added utilities to create topological sort node lists

---
+++ TopologicalSort/actions/PredecessorAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace J4JSoftware.Utilities
+{
+    [ AttributeUsage( AttributeTargets.Class, AllowMultiple = false, Inherited = false ) ]
+    public class PredecessorAttribute : Attribute
+    {
+        public PredecessorAttribute(
+            Type? predecessor
+        )
+        {
+            Predecessor = predecessor;
+        }
+
+        public Type? Predecessor { get; }
+    }
+}
---
+++ TopologicalSort/actions/PredecessorList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using J4JSoftware.Logging;
+
+namespace J4JSoftware.Utilities
+{
+    public static class TopologicalSortExtensions
+    {
+        public static Nodes<T> ToNodeList<T>( 
+            this IEnumerable<T> source, 
+            IEqualityComparer<T>? comparer = null,
+            IJ4JLogger? logger = null )
+            where T: class, IEquatable<T>
+        {
+            var retVal = new Nodes<T>( comparer );
+
+            var idx = 0;
+            var nodeList = source.ToList();
+
+            foreach( var node in nodeList )
+            {
+                var nodeType = node.GetType();
+
+                var predAttr = nodeType.GetCustomAttribute<PredecessorAttribute>(false);
+                if( predAttr == null )
+                {
+                    logger?.Error("Node type '{0}' (item {1}) is not decorated with a PredecessorAttribute", nodeType, idx );
+                    continue;
+                }
+
+                if( predAttr.Predecessor == null )
+                    retVal.AddIndependentNode( node );
+                else
+                {
+                    var predecessor = nodeList.FirstOrDefault(x => x.GetType() == predAttr.Predecessor);
+                    if (predecessor == null)
+                    {
+                        logger?.Fatal<string>( "Couldn't find predecessor extractor {0}", predAttr.Predecessor.Name );
+                        continue;
+                    }
+
+                    retVal.AddDependentNode(node, predecessor);
+                }
+
+                idx++;
+            }
+
+            return retVal;
+        }
+    }
+}
