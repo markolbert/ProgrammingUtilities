e0660c0ca4981d9644ec37cdad12ac3b970b6c94
Mark Olbert <mark@arcabama.com>
2022-03-16 10:30:25 -0700

    cleanup

--- DependencyInjection/DIExtensions.cs
+++ DependencyInjection/DIExtensions.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Autofac;
-using J4JSoftware.Logging;
 
 namespace J4JSoftware.DependencyInjection
 {
