1cb195242ecfb46ddb0756f0c9e65ea390d4fd67
Mark Olbert <mark@arcabama.com>
2022-04-10 09:51:33 -0700

    support WinApps (WinUI 3)

---
+++ WindowsAppUtilities/J4JWinAppHostConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using J4JSoftware.DependencyInjection;
+
+namespace J4JSoftware.WindowsAppUtilities
+{
+    public class J4JWinAppHostConfiguration : J4JHostConfiguration
+    {
+        public J4JWinAppHostConfiguration(
+            bool registerHost = true
+        )
+            : base( AppEnvironment.PackagedWinApp, registerHost )
+        {
+            ApplicationConfigurationFolder = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+        }
+
+        public override string UserConfigurationFolder => ApplicationConfigurationFolder;
+    }
+}
