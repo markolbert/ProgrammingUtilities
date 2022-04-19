87ca8bd7cd09ac81ba42b9192970e823b324e949
Mark Olbert <mark@arcabama.com>
2022-04-19 07:39:41 -0700

    add project for WindowsApp/WinUI3 utilities

---
+++ WindowsAppUtilities/ITextFileLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace J4JSoftware.WindowsAppUtilities
+{
+    public interface ITextFileLogger
+    {
+        void Log(
+            string text,
+            [ CallerMemberName ] string calledBy = "",
+            [ CallerFilePath ] string callerFilePath = "",
+            [ CallerLineNumber ] int lineNum = 0
+        );
+
+        void Log(
+            Exception exception,
+            [ CallerMemberName ] string calledBy = "",
+            [ CallerFilePath ] string callerFilePath = "",
+            [ CallerLineNumber ] int lineNum = 0
+        );
+    }
+
+}
---
+++ WindowsAppUtilities/TextFileLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace J4JSoftware.WindowsAppUtilities;
+
+public class TextFileLogger : ITextFileLogger
+{
+    private readonly string _logFile;
+
+    public TextFileLogger()
+    {
+        _logFile = System.IO.Path.Combine(Windows.Storage.ApplicationData.Current.LocalFolder.Path, "bullshit.txt");
+    }
+
+    public void Log(
+        string text,
+        [ CallerMemberName ] string calledBy = "",
+        [ CallerFilePath ] string callerFilePath = "",
+        [ CallerLineNumber ] int lineNum = 0
+    )
+    {
+        File.AppendAllText( _logFile, $"{calledBy}\t{text}\n\t{callerFilePath}:{lineNum}\n" );
+    }
+
+    public void Log(
+        Exception e,
+        [ CallerMemberName ] string calledBy = "",
+        [ CallerFilePath ] string callerFilePath = "",
+        [ CallerLineNumber ] int lineNum = 0
+    ) =>
+        Log( $"Exception {e.GetType().Name} - {e.Message}", calledBy, callerFilePath, lineNum );
+}
