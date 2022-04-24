4228093b55ff8d006d6e689a02c3670c0ccba0ba
Mark Olbert <mark@arcabama.com>
2022-04-24 12:06:08 -0700

    move J4JDeusEx to separate assembly

---
+++ DeusEx/J4JDeusEx.cs
@@ -0,0 +1,76 @@
+﻿using J4JSoftware.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace J4JSoftware.DeusEx;
+
+public class J4JDeusEx
+{
+    public static IServiceProvider? ServiceProvider { get; protected set; }
+
+    public static bool IsInitialized { get; protected set; }
+    public static string? CrashFilePath { get; protected set; }
+    public static IJ4JLogger? Logger { get; protected set; }
+
+    public static IJ4JLogger? GetLogger<T>()
+    {
+        if( ServiceProvider == null )
+            return null;
+
+        var retVal = ServiceProvider.GetService<IJ4JLogger>();
+        retVal?.SetLoggedType<T>();
+
+        return retVal;
+    }
+
+    public static bool Initialize(
+        IServiceProvider serviceProvider,
+        string? crashFilePath = null
+    )
+    {
+        CrashFilePath = string.IsNullOrEmpty(crashFilePath)
+            ? Path.Combine(Environment.CurrentDirectory, "crashFile.txt")
+            : crashFilePath;
+
+        ServiceProvider = serviceProvider;
+        Logger = ServiceProvider.GetService<IJ4JLogger>();
+
+        IsInitialized = true;
+
+        return true;
+    }
+
+    public static void OutputFatalMessage( string msg, IJ4JLogger? logger )
+    {
+        // how we log depends on whether we successfully created the service provider
+        logger?.Fatal( msg );
+
+        if( !FileIsWriteable( CrashFilePath ) )
+            return;
+
+        try
+        {
+            File.AppendAllText( CrashFilePath!, $"{msg}\n" );
+        }
+        catch
+        {
+        }
+    }
+
+    private static bool FileIsWriteable(string? filePath)
+    {
+        if( string.IsNullOrEmpty( filePath ) )
+            return false;
+
+        try
+        {
+            File.AppendAllText(filePath, "test");
+            File.Delete(filePath);
+        }
+        catch
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ DeusEx/J4JDeusExException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J4JSoftware.DeusEx
+{
+    public class J4JDeusExException : Exception
+    {
+        public J4JDeusExException( string msg, Exception? innerException = null )
+            : base( msg, innerException )
+        {
+        }
+    }
+}
