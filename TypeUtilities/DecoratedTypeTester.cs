7e30b9c7a61429a3ab32b35a82234d290c9662ed
Mark Olbert <mark@arcabama.com>
2022-08-07 12:47:03 -0700

    added new tests

---
+++ TypeUtilities/DecoratedTypeTester.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace J4JSoftware.DependencyInjection;
+
+public class DecoratedTypeTester<T> : ConstructorTesterBase<T>
+    where T : class
+{
+    private readonly bool _allowInherited;
+    private readonly List<Type> _requiredAttributes;
+
+    public DecoratedTypeTester(bool allowInherited, params Type[] requiredAttributes )
+    {
+        _allowInherited = allowInherited;
+
+        _requiredAttributes = requiredAttributes.Where( t => typeof( Attribute ).IsAssignableFrom( t ) )
+                                                .ToList();
+    }
+
+    public override bool MeetsRequirements( Type toCheck )
+    {
+        if( !base.MeetsRequirements( toCheck ) )
+            return false;
+
+        var typeAttributes = toCheck.GetCustomAttributes( _allowInherited );
+
+        foreach( var attributeType in _requiredAttributes )
+        {
+            if( !typeAttributes.Any( x => attributeType.IsAssignableFrom( x.GetType() ) ) )
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ TypeUtilities/FilterTypeTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace J4JSoftware.DependencyInjection;
+
+public class FilterTypeTester<T> : ConstructorTesterBase<T>
+    where T : class
+{
+    private readonly Func<Type, bool> _filter;
+
+    public FilterTypeTester(
+        Func<Type, bool> filter
+        )
+    {
+        _filter = filter;
+    }
+
+    public override bool MeetsRequirements( Type toCheck ) => base.MeetsRequirements( toCheck ) && _filter( toCheck );
+}
