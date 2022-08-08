77973db11023ab1cafc5f2574e5026080baca3c3
Mark Olbert <mark@arcabama.com>
2022-08-08 09:35:34 -0700

    moved TypeTest adding routines to extension methods

--- TypeUtilities/TypeTests.cs
+++ TypeUtilities/TypeTests.cs
@@ -10,64 +10,13 @@ namespace J4JSoftware.DependencyInjection;
 public class TypeTests<T> : IEnumerable<ITypeTester>
     where T : class
 {
-    private readonly List<ITypeTester> _tests = new();
+    public Type TypeToTest { get; } = typeof(T);
 
-    public TypeTests<T> AddTests( params PredefinedTypeTests[] predefined ) =>
-        AddTests( predefined.AsEnumerable() );
-
-    public TypeTests<T> AddTests( IEnumerable<PredefinedTypeTests> predefined )
-    {
-        foreach( var curItem in predefined )
-        {
-            switch (curItem)
-            {
-                case PredefinedTypeTests.ParameterlessConstructor:
-                    _tests.Add(new ConstructorParameterTester<T>());
-                    break;
-
-                case PredefinedTypeTests.OnlyJ4JLoggerRequired:
-                    _tests.Add(new ConstructorParameterTester<T>(typeof(IJ4JLogger)));
-                    break;
-
-                case PredefinedTypeTests.NonAbstract:
-                    _tests.Add(TypeTester.NonAbstract);
-                    break;
-
-                case PredefinedTypeTests.NonGeneric:
-                    _tests.Add(TypeTester.NonGeneric);
-                    break;
-
-                default:
-                    throw new
-                        InvalidEnumArgumentException($"Unsupported {nameof(PredefinedTypeTests)} value '{curItem}'");
-            }
-        }
-
-        return this;
-    }
-
-    public TypeTests<T> HasRequiredConstructor( params Type[] ctorParameters ) =>
-        HasRequiredConstructor( ctorParameters.AsEnumerable() );
-
-    public TypeTests<T> HasRequiredConstructor( IEnumerable<Type> ctorParameters )
-    {
-        _tests.Add(new ConstructorParameterTester<T>(ctorParameters)  );
-
-        return this;
-    }
-
-    public TypeTests<T> AddTests( params ITypeTester[] tests ) => AddTests( tests.AsEnumerable() );
-
-    public TypeTests<T> AddTests( IEnumerable<ITypeTester> tests )
-    {
-        _tests.AddRange(tests);
-
-        return this;
-    }
+    internal List<ITypeTester> Tests { get; } = new();
 
     public IEnumerator<ITypeTester> GetEnumerator()
     {
-        foreach( var test in _tests )
+        foreach( var test in Tests )
         {
             yield return test;
         }
