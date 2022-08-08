54cc37008ef0e003a58fb8e514e0f446573f5bc6
Mark Olbert <mark@arcabama.com>
2022-08-08 09:36:24 -0700

    moved TypeTest adding methods to extension methods

--- TypeUtilities/TypeTestExtensions.cs
+++ TypeUtilities/TypeTestExtensions.cs
@@ -4,64 +4,159 @@ using System.ComponentModel;
 using System.Linq;
 using J4JSoftware.Logging;
 
-namespace J4JSoftware.DependencyInjection
+namespace J4JSoftware.DependencyInjection;
+
+public static class TypeTestExtensions
 {
-    public static class TypeTestExtensions
+    #region Add tests methods
+
+    public static TypeTests<T> AddPredefinedTests<T>( this TypeTests<T> tester, params PredefinedTypeTests[] predefined )
+        where T : class =>
+        tester.AddPredefinedTests( predefined.AsEnumerable() );
+
+    public static TypeTests<T> AddPredefinedTests<T>( this TypeTests<T> tester, IEnumerable<PredefinedTypeTests> predefined )
+        where T : class
     {
-        public static IEnumerable<Type> MeetRequirements<T>( this IEnumerable<Type> types,
-                                                             params ITypeTester[] tests )
-            where T : class =>
-            types.Where( t => typeof( T ).IsAssignableFrom( t )
-                              && tests.All( x => x.MeetsRequirements( t ) ) );
-
-        public static IEnumerable<Type> MeetRequirements<T>( this IEnumerable<Type> types,
-                                                             IEnumerable<ITypeTester> tests )
-            where T : class =>
-            types.Where( t => typeof( T ).IsAssignableFrom( t )
-                              && tests.All( x => x.MeetsRequirements( t ) ) );
-
-        public static IEnumerable<Type> MeetRequirements<T>( this IEnumerable<Type> types,
-                                                             params PredefinedTypeTests[] predefinedTests )
-            where T : class =>
-            types.MeetRequirements<T>( predefinedTests.ToTypeTesters<T>() );
-
-        public static IEnumerable<Type> MeetRequirements<T>( this IEnumerable<Type> types,
-                                                             IEnumerable<PredefinedTypeTests> predefinedTests )
-            where T : class =>
-            types.MeetRequirements<T>( predefinedTests.ToTypeTesters<T>() );
-
-        private static List<ITypeTester> ToTypeTesters<T>( this IEnumerable<PredefinedTypeTests> predefinedTests )
-            where T : class
+        foreach( var curItem in predefined )
         {
-            var retVal = new List<ITypeTester>();
-
-            foreach ( var test in predefinedTests.Distinct() )
+            switch( curItem )
             {
-                switch ( test )
-                {
-                    case PredefinedTypeTests.ParameterlessConstructor:
-                        retVal.Add( new ConstructorParameterTester<T>() );
-                        break;
+                case PredefinedTypeTests.ParameterlessConstructor:
+                    tester.Tests.Add( new ConstructorParameterTester<T>() );
+                    break;
 
-                    case PredefinedTypeTests.OnlyJ4JLoggerRequired:
-                        retVal.Add( new ConstructorParameterTester<T>( typeof( IJ4JLogger ) ) );
-                        break;
+                case PredefinedTypeTests.OnlyJ4JLoggerRequired:
+                    tester.Tests.Add( new ConstructorParameterTester<T>( typeof( IJ4JLogger ) ) );
+                    break;
 
-                    case PredefinedTypeTests.NonAbstract:
-                        retVal.Add( TypeTester.NonAbstract );
-                        break;
+                case PredefinedTypeTests.NonAbstract:
+                    tester.Tests.Add( TypeTester.NonAbstract );
+                    break;
 
-                    case PredefinedTypeTests.NonGeneric:
-                        retVal.Add( TypeTester.NonGeneric );
-                        break;
+                case PredefinedTypeTests.NonGeneric:
+                    tester.Tests.Add( TypeTester.NonGeneric );
+                    break;
 
-                    default:
-                        throw new
-                            InvalidEnumArgumentException( $"Unsupported {nameof( PredefinedTypeTests )} value '{test}'" );
-                }
+                default:
+                    throw new
+                        InvalidEnumArgumentException(
+                            $"Unsupported {nameof( PredefinedTypeTests )} value '{curItem}'" );
             }
-
-            return retVal;
         }
+
+        return tester;
+    }
+
+    public static TypeTests<T> HasConstructorArgs<T>( this TypeTests<T> tester, params Type[] ctorParameters )
+        where T : class =>
+        tester.HasConstructorArgs( ctorParameters.AsEnumerable() );
+
+    public static TypeTests<T> HasConstructorArgs<T>( this TypeTests<T> tester, IEnumerable<Type> ctorParameters )
+        where T : class
+    {
+        tester.Tests.Add( new ConstructorParameterTester<T>( ctorParameters ) );
+
+        return tester;
+    }
+
+    public static TypeTests<T> DecoratedWith<T>(
+        this TypeTests<T> tester,
+        Type requiredAttribute,
+        bool allowInherited = false
+    )
+        where T : class
+    {
+        tester.Tests.Add( new DecoratedTypeTester<T>( allowInherited, requiredAttribute ) );
+
+        return tester;
+    }
+
+    public static TypeTests<T> FilteredBy<T>(
+        this TypeTests<T> tester,
+        Func<Type, bool> filter
+    )
+        where T : class
+    {
+        tester.Tests.Add(new FilterTypeTester<T>(filter)  );
+
+        return tester;
+    }
+
+    public static TypeTests<T> AddTests<T>( this TypeTests<T> tester, params ITypeTester[] tests )
+        where T : class
+    {
+        tester.Tests.AddRange(tests);
+
+        return tester;
+    }
+
+    #endregion
+
+    #region MeetsRequirements methods
+
+    public static IEnumerable<Type> MeetRequirements<T>(
+        this IEnumerable<Type> types,
+        params ITypeTester[] tests
+    )
+        where T : class =>
+        types.Where( t => typeof( T ).IsAssignableFrom( t )
+                      && tests.All( x => x.MeetsRequirements( t ) ) );
+
+    public static IEnumerable<Type> MeetRequirements<T>(
+        this IEnumerable<Type> types,
+        IEnumerable<ITypeTester> tests
+    )
+        where T : class =>
+        types.Where( t => typeof( T ).IsAssignableFrom( t )
+                      && tests.All( x => x.MeetsRequirements( t ) ) );
+
+    public static IEnumerable<Type> MeetRequirements<T>(
+        this IEnumerable<Type> types,
+        params PredefinedTypeTests[] predefinedTests
+    )
+        where T : class =>
+        types.MeetRequirements<T>( predefinedTests.ToTypeTesters<T>() );
+
+    public static IEnumerable<Type> MeetRequirements<T>(
+        this IEnumerable<Type> types,
+        IEnumerable<PredefinedTypeTests> predefinedTests
+    )
+        where T : class =>
+        types.MeetRequirements<T>( predefinedTests.ToTypeTesters<T>() );
+
+    #endregion
+
+    private static List<ITypeTester> ToTypeTesters<T>( this IEnumerable<PredefinedTypeTests> predefinedTests )
+        where T : class
+    {
+        var retVal = new List<ITypeTester>();
+
+        foreach( var test in predefinedTests.Distinct() )
+        {
+            switch( test )
+            {
+                case PredefinedTypeTests.ParameterlessConstructor:
+                    retVal.Add( new ConstructorParameterTester<T>() );
+                    break;
+
+                case PredefinedTypeTests.OnlyJ4JLoggerRequired:
+                    retVal.Add( new ConstructorParameterTester<T>( typeof( IJ4JLogger ) ) );
+                    break;
+
+                case PredefinedTypeTests.NonAbstract:
+                    retVal.Add( TypeTester.NonAbstract );
+                    break;
+
+                case PredefinedTypeTests.NonGeneric:
+                    retVal.Add( TypeTester.NonGeneric );
+                    break;
+
+                default:
+                    throw new
+                        InvalidEnumArgumentException( $"Unsupported {nameof( PredefinedTypeTests )} value '{test}'" );
+            }
+        }
+
+        return retVal;
     }
 }
