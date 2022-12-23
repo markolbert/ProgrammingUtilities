89ee5bc7f5f9931a9da9dfd1b0eb8b0c28a5a22c
Mark Olbert <mark@arcabama.com>
2022-12-23 08:50:55 -0800

    add extension method to retrieve root-level expression property

---
+++ MiscellaneousUtilities/ExpressionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+using System;
+
+namespace J4JSoftware.Utilities;
+
+public static class ExpressionExtensions
+{
+    public static PropertyInfo GetPropertyInfo<TContainer, TProp>(
+        this Expression<Func<TContainer, TProp>> expression
+    ) =>
+        expression.Body switch
+        {
+            null => throw new ArgumentNullException( nameof( expression ) ),
+            UnaryExpression ue when ue.Operand is MemberExpression me => (PropertyInfo) me.Member,
+            MemberExpression me => (PropertyInfo) me.Member,
+            _ => throw new ArgumentException( $"The expression isn't a valid property. [ {expression} ]" )
+        };
+}
