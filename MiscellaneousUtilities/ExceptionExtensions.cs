b4c7cc013b56b2fa106a15cd4c971e1d48fa05a3
Mark Olbert <mark@arcabama.com>
2022-12-29 10:07:08 -0800

    relocated exception formatting extension method

---
+++ MiscellaneousUtilities/ExceptionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J4JSoftware.Utilities;
+
+public static class ExceptionExtensions
+{
+    public static string FormatException(
+        this Exception e,
+        string message,
+        [CallerMemberName] string callerName = "",
+        [CallerFilePath] string callerFilePath = "",
+        [CallerLineNumber] int callerLineNum = 0
+    )
+    {
+        var retVal = new StringBuilder(message);
+
+        retVal.Append($"\n\tException type:\t{e.GetType().Name}");
+        retVal.Append($"\n\tDetails:\t{e.Message}");
+
+        if (e.InnerException != null)
+            retVal.Append($"\n\tInner Details:\t{e.InnerException.Message}");
+
+        retVal.Append($"\n\n\tCalled by:\t{callerName}");
+        retVal.Append($"\n\tSource file:\t{callerFilePath}");
+        retVal.Append($"\n\tLine number:\t{callerLineNum}");
+
+        return retVal.ToString();
+    }
+}
