2c8e762666d7e6870e3766373e3970a3446cddb0
Mark Olbert <mark@arcabama.com>
2023-02-10 18:52:12 -0800

    fix use of wrong check

---
+++ TypeUtilities/IsAssignableTo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace J4JSoftware.DependencyInjection;
+
+public class IsAssignableTo<T> : ITypeTester
+    where T : class
+{
+    public bool MeetsRequirements( Type toCheck ) => toCheck.IsAssignableTo( typeof( T ) );
+}
