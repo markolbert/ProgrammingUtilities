488082cb8e639e8ba22c4392eeb56b87dbc54f7d
Mark Olbert <mark@arcabama.com>
2023-02-12 11:49:24 -0800

    2D rectangle tests

---
+++ Test.VisualUtilities/Rectangle2DTestData.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using J4JSoftware.VisualUtilities;
+
+namespace Test.VisualUtilities;
+
+public class Rectangle2DTestData
+{
+    public record RectParams( float Height, float Width, float Rotation, Vector3 Center );
+
+    public record RectTestParams(
+            float Height,
+            float Width,
+            float Rotation,
+            Vector3 Center,
+            Vector3[] RectangleCorners
+        )
+        : RectParams( Height,
+                      Width,
+                      Rotation,
+                      Center );
+
+    public record InsideOutsideRectParams( RectParams Outer, RectParams Inner, ConvexPolygonPosition Relationship );
+
+    public static IEnumerable<object?[]> GetTestRectangles()
+    {
+        yield return new object?[]
+        {
+            new RectTestParams( 512,
+                            512,
+                            0,
+                            new Vector3( 0, 0, 0 ),
+                            new[]
+                            {
+                                new Vector3( -256, -256, 0 ),
+                                new Vector3( -256, 256, 0 ),
+                                new Vector3( 256, 256, 0 ),
+                                new Vector3( 256, -256, 0 ),
+                            } )
+        };
+
+        yield return new object?[]
+        {
+            new RectTestParams( 512,
+                            512,
+                            315,
+                            new Vector3( 0, 0, 0 ),
+                            new[]
+                            {
+                                new Vector3( -362.04F, 0, 0 ),
+                                new Vector3( 0, 362.04F, 0 ),
+                                new Vector3( 362.04F, 0, 0 ),
+                                new Vector3( 0, -362.04F, 0 ),
+                            } )
+        };
+
+        yield return new object?[]
+        {
+            new RectTestParams( 300,
+                            750,
+                            151,
+                            new Vector3( 73, -129, 0 ),
+                            new[]
+                            {
+                                new Vector3( -327.704F, -78.389F, 0 ),
+                                new Vector3( -182.261F, 183.997F, 0 ),
+                                new Vector3( 473.704F, -179.611F, 0 ),
+                                new Vector3( 328.261F, -441.997F, 0 ),
+                            } )
+        };
+    }
+
+    public static IEnumerable<object?[]> GetInsideOutside()
+    {
+        yield return new object?[]
+        {
+            new InsideOutsideRectParams( new RectTestParams( 512,
+                                                         512,
+                                                         0,
+                                                         new Vector3( 0, 0, 0 ),
+                                                         new[]
+                                                         {
+                                                             new Vector3( -256, 256, 0 ),
+                                                             new Vector3( 256, 256, 0 ),
+                                                             new Vector3( 256, -256, 0 ),
+                                                             new Vector3( -256, -256, 0 )
+                                                         } ),
+                                     new RectTestParams( 256,
+                                                         256,
+                                                         0,
+                                                         new Vector3( 0, 0, 0 ),
+                                                         new[]
+                                                         {
+                                                             new Vector3( -128, 128, 0 ),
+                                                             new Vector3( 128, 128, 0 ),
+                                                             new Vector3( 128, -128, 0 ),
+                                                             new Vector3( -128, -128, 0 )
+                                                         } ),
+                                     ConvexPolygonPosition.Inside )
+        };
+
+        yield return new object?[]
+        {
+            new InsideOutsideRectParams( new RectTestParams( 512,
+                                                         512,
+                                                         0,
+                                                         new Vector3( 0, 0, 0 ),
+                                                         new[]
+                                                         {
+                                                             new Vector3( -256, 256, 0 ),
+                                                             new Vector3( 256, 256, 0 ),
+                                                             new Vector3( 256, -256, 0 ),
+                                                             new Vector3( -256, -256, 0 )
+                                                         } ),
+                                     new RectTestParams( 512,
+                                                         512,
+                                                         0,
+                                                         new Vector3( 0, 0, 0 ),
+                                                         new[]
+                                                         {
+                                                             new Vector3( -256, 256, 0 ),
+                                                             new Vector3( 256, 256, 0 ),
+                                                             new Vector3( 256, -256, 0 ),
+                                                             new Vector3( -256, -256, 0 )
+                                                         } ),
+                                     ConvexPolygonPosition.Same )
+        };
+
+        yield return new object?[]
+        {
+            new InsideOutsideRectParams( new RectTestParams( 512,
+                                                         512,
+                                                         0,
+                                                         new Vector3( 0, 0, 0 ),
+                                                         new[]
+                                                         {
+                                                             new Vector3( -256, 256, 0 ),
+                                                             new Vector3( 256, 256, 0 ),
+                                                             new Vector3( 256, -256, 0 ),
+                                                             new Vector3( -256, -256, 0 )
+                                                         } ),
+                                     new RectTestParams( 256,
+                                                         256,
+                                                         0,
+                                                         new Vector3( 512, 512, 0 ),
+                                                         new[]
+                                                         {
+                                                             new Vector3( -128, 128, 0 ),
+                                                             new Vector3( 128, 128, 0 ),
+                                                             new Vector3( 128, -128, 0 ),
+                                                             new Vector3( -128, -128, 0 )
+                                                         } ),
+                                     ConvexPolygonPosition.Outside )
+        };
+
+    }
+}
---
+++ Test.VisualUtilities/RectangleTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using J4JSoftware.VisualUtilities;
+using Xunit;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Test.VisualUtilities;
+
+public class RectangleTests
+{
+    [Theory]
+    [MemberData(nameof(Rectangle2DTestData.GetTestRectangles), MemberType = typeof(Rectangle2DTestData))]
+    public void Corners( Rectangle2DTestData.RectTestParams data )
+    {
+        var rectangle = new Rectangle2D( data.Height, data.Width, data.Rotation, data.Center );
+
+        for( var idx = 0; idx < 4; idx++ )
+        {
+            CheckPointEquivalence(rectangle[idx], data.RectangleCorners[idx], 1E-2F);
+        }
+    }
+
+    [Theory]
+    [MemberData(nameof(Rectangle2DTestData.GetInsideOutside), MemberType = typeof(Rectangle2DTestData))]
+    public void InsideOutsideRectangle( Rectangle2DTestData.InsideOutsideRectParams data )
+    {
+        var outer = new Rectangle2D(data.Outer.Height, data.Outer.Width, data.Outer.Rotation, data.Outer.Center);
+        var inner = new Rectangle2D(data.Inner.Height, data.Inner.Width, data.Inner.Rotation, data.Inner.Center);
+
+        outer.Contains( inner ).Should().Be( data.Relationship );
+    }
+
+    private void CheckPointEquivalence( Vector3 point1, Vector3 point2, float precision )
+    {
+        point1.X.Should().BeInRange(point2.X - precision, point2.X + precision);
+        point1.Y.Should().BeInRange(point2.Y - precision, point2.Y + precision);
+    }
+}
