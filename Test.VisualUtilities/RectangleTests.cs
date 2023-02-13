73a514eb839da325ef4e6120658ec16b644804ca
Mark Olbert <mark@arcabama.com>
2023-02-13 14:37:08 -0800

    test cartesian/display transformations

--- Test.VisualUtilities/RectangleTests.cs
+++ Test.VisualUtilities/RectangleTests.cs
@@ -34,4 +34,43 @@ public class RectangleTests
         point1.X.Should().BeInRange(point2.X - precision, point2.X + precision);
         point1.Y.Should().BeInRange(point2.Y - precision, point2.Y + precision);
     }
+
+    [ Theory ]
+    [ InlineData( 100, 200, 0, 0, 0, 5000, 5000 ) ]
+    [ InlineData( 100, 200, 45, 0, 0, 5000, 5000 ) ]
+    [InlineData(100, 200, 0, 50, 50, 5000, 5000)]
+    [InlineData(100, 200, 45, 50, 50, 5000, 5000)]
+    [InlineData(100, 200, 0, -50, -50, 5000, 5000)]
+    [InlineData(100, 200, 45, -50, -50, 5000, 5000)]
+    [InlineData(100, 200, 0, -50, 50, 5000, 5000)]
+    [InlineData(100, 200, 45, -50, 50, 5000, 5000)]
+    [InlineData(100, 200, 0, 50, -50, 5000, 5000)]
+    [InlineData(100, 200, 45, 50, -50, 5000, 5000)]
+    public void DisplaySpace(
+        float height,
+        float width,
+        float rotation,
+        float centerX,
+        float centerY,
+        float displayHeight,
+        float displayWidth
+    )
+    {
+        var initialRect = new Rectangle2D( height, width, rotation, new Vector3( centerX, centerY, 0 ) );
+        var viewport = new Vector3( displayHeight, displayWidth, 0 );
+        var displayRect = initialRect.ToDisplaySpace( viewport );
+
+        var reversedRect = displayRect.FromDisplaySpace( viewport );
+
+        for( var idx = 0; idx < 4; idx++ )
+        {
+            reversedRect[ idx ]
+               .X.Should()
+               .BeApproximately( initialRect[ idx ].X, 0.001F );
+
+            reversedRect[idx]
+               .Y.Should()
+               .BeApproximately(initialRect[idx].Y, 0.001F);
+        }
+    }
 }
