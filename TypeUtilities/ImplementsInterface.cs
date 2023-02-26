65881e9823336c79546b532db2e20ec855a3c73b
Mark Olbert <mark@arcabama.com>
2023-02-26 09:47:07 -0800

    add test

---
+++ TypeUtilities/ImplementsInterface.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace J4JSoftware.DependencyInjection;
+
+public class ImplementsInterface<T> : ITypeTester
+    where T : class
+{
+    public bool MeetsRequirements( Type toCheck ) =>
+        typeof(T).GetInterface( toCheck.FullName ?? string.Empty ) != null;
+}
