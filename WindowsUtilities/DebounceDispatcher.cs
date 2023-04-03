c16a6dc25384050ae367446c5b1ed3aab304e7f0
Mark Olbert <mark@arcabama.com>
2023-04-03 13:57:01 -0700

    extract non-DeusEx stuff to separate assembly

---
+++ WindowsUtilities/DebounceDispatcher.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) 2021, 2022 Mark A. Olbert 
+// 
+// This file is part of WPFUtilities.
+//
+// WPFUtilities is free software: you can redistribute it and/or modify it 
+// under the terms of the GNU General Public License as published by the 
+// Free Software Foundation, either version 3 of the License, or 
+// (at your option) any later version.
+// 
+// WPFUtilities is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+// 
+// You should have received a copy of the GNU General Public License along 
+// with WPFUtilities. If not, see <https://www.gnu.org/licenses/>.
+
+using System;
+using Microsoft.UI.Xaml;
+
+namespace J4JSoftware.WindowsUtilities;
+
+// thanx to Rick Strahl for this one.
+// https://weblog.west-wind.com/posts/2017/jul/02/debouncing-and-throttling-dispatcher-events
+public class DebounceDispatcher
+{
+    public DebounceDispatcher(
+        int interval,
+        Action<object?> action,
+        object? optParam = null
+        )
+    {
+        var timer = new DispatcherTimer();
+        timer.Tick += ( _, _ ) => action.Invoke( optParam );
+        timer.Interval = TimeSpan.FromMilliseconds(interval);
+        timer.Start();
+    }
+}
