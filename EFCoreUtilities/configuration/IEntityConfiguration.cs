9429e98547bff78b07a70ef7ada5a8a73f10d99b
Mark Olbert <mark@arcabama.com>
2023-04-04 17:28:13 -0700

    add/update license

--- EFCoreUtilities/configuration/IEntityConfiguration.cs
+++ EFCoreUtilities/configuration/IEntityConfiguration.cs
@@ -1,19 +1,21 @@
-﻿// Copyright (c) 2021, 2022 Mark A. Olbert 
+﻿#region copyright
+// Copyright (c) 2021, 2022, 2023 Mark A. Olbert 
 // 
-// This file is part of EFCoreUtilities.
+// This file is part of J4JLogger.
 //
-// EFCoreUtilities is free software: you can redistribute it and/or modify it 
+// J4JLogger is free software: you can redistribute it and/or modify it 
 // under the terms of the GNU General Public License as published by the 
 // Free Software Foundation, either version 3 of the License, or 
 // (at your option) any later version.
 // 
-// EFCoreUtilities is distributed in the hope that it will be useful, but 
+// J4JLogger is distributed in the hope that it will be useful, but 
 // WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
 // or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
 // for more details.
 // 
 // You should have received a copy of the GNU General Public License along 
-// with EFCoreUtilities. If not, see <https://www.gnu.org/licenses/>.
+// with J4JLogger. If not, see <https://www.gnu.org/licenses/>.
+#endregion
 
 using Microsoft.EntityFrameworkCore;
 
--- ExcelExport/TableOrientation.cs
+++ ExcelExport/TableOrientation.cs
@@ -1,19 +1,21 @@
-﻿// Copyright (c) 2021, 2022 Mark A. Olbert 
+﻿#region copyright
+// Copyright (c) 2021, 2022, 2023 Mark A. Olbert 
 // 
-// This file is part of ExcelExport.
+// This file is part of J4JLogger.
 //
-// ExcelExport is free software: you can redistribute it and/or modify it 
+// J4JLogger is free software: you can redistribute it and/or modify it 
 // under the terms of the GNU General Public License as published by the 
 // Free Software Foundation, either version 3 of the License, or 
 // (at your option) any later version.
 // 
-// ExcelExport is distributed in the hope that it will be useful, but 
+// J4JLogger is distributed in the hope that it will be useful, but 
 // WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
 // or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
 // for more details.
 // 
 // You should have received a copy of the GNU General Public License along 
-// with ExcelExport. If not, see <https://www.gnu.org/licenses/>.
+// with J4JLogger. If not, see <https://www.gnu.org/licenses/>.
+#endregion
 
 namespace J4JSoftware.Excel;
 
