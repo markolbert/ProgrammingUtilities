b928e31cec43363ed058174e9c5dd610654de1a1
Mark Olbert <mark@arcabama.com>
2023-04-12 09:58:17 -0700

    update license text

--- VisualUtilities/Rectangle2D.cs
+++ VisualUtilities/Rectangle2D.cs
@@ -1,20 +1,22 @@
 ﻿#region copyright
 // Copyright (c) 2021, 2022, 2023 Mark A. Olbert 
-// 
-// This file is part of J4JLogger.
+// https://www.JumpForJoySoftware.com
+// Rectangle2D.cs
 //
-// J4JLogger is free software: you can redistribute it and/or modify it 
+// This file is part of JumpForJoy Software's VisualUtilities.
+// 
+// VisualUtilities is free software: you can redistribute it and/or modify it 
 // under the terms of the GNU General Public License as published by the 
 // Free Software Foundation, either version 3 of the License, or 
 // (at your option) any later version.
 // 
-// J4JLogger is distributed in the hope that it will be useful, but 
+// VisualUtilities is distributed in the hope that it will be useful, but 
 // WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
 // or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
 // for more details.
 // 
 // You should have received a copy of the GNU General Public License along 
-// with J4JLogger. If not, see <https://www.gnu.org/licenses/>.
+// with VisualUtilities. If not, see <https://www.gnu.org/licenses/>.
 #endregion
 
 using System;
