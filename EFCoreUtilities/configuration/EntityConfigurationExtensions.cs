92d437bd84f6da7599663f6bfe3b8d314a4475b3
Mark Olbert <mark@arcabama.com>
2024-01-30 09:33:48 -0800

    change name to avoid confusion

--- EFCoreUtilities/configuration/EntityConfigurationExtensions.cs
+++ EFCoreUtilities/configuration/EntityConfigurationExtensions.cs
@@ -20,8 +20,10 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Text.Json.Serialization.Metadata;
 using Microsoft.EntityFrameworkCore;
 
 namespace J4JSoftware.EFCoreUtilities;
@@ -56,7 +58,7 @@ public static class EntityConfigurationExtensions
         }
     }
 
-    public static void ConfigureDbContextEntities<TContext>(this ModelBuilder modelBuilder)
+    public static void ConfigureEntitiesFromAttributes<TContext>(this ModelBuilder modelBuilder)
         where TContext: DbContext
     {
         foreach (var configAttr in typeof(TContext).GetProperties()
@@ -75,6 +77,64 @@ public static class EntityConfigurationExtensions
         {
             configAttr?.GetConfigurator().Configure(modelBuilder);
         }
+    }
+
+    public static bool ConfigureEntitiesFromDbContext<TContext>(
+        this ModelBuilder modelBuilder,
+        out List<Type> unconfigured,
+        params Assembly[] assemblies
+    )
+        where TContext : DbContext
+    {
+        // we should be configuring every entity type in TContext, so get a list of all the
+        // TContext DbSet<>s and extract their entity types. Note that we have to do this by
+        // reflection because GetEntityTypes() depends on a fully-configured DbContext Model,
+        // and that hasn't been built yet at this point.
+        unconfigured = DbExtensions.GetEntityTypes<TContext>();
+
+        // if no assemblies were specified, assume we should only look in the calling assembly
+        if( assemblies.Length == 0 )
+            assemblies = new[] { Assembly.GetCallingAssembly() };
+
+        // find all the IEntityConfigurator types in the assemblies
+        foreach( var configType in assemblies.SelectMany( a => a.GetTypes() )
+                                             .Where( t =>
+                                              {
+                                                  // only interested in types that support IEntityConfiguration
+                                                  if( t.GetInterface( nameof( IEntityConfiguration ) ) == null )
+                                                      return false;
+
+                                                  // the IEntityConfiguration type must have a public 
+                                                  // parameterless constructor
+                                                  return t.GetConstructors()
+                                                          .Any( ctor => ctor.GetParameters().Length == 0 );
+                                              } ) )
+        {
+            var configurator = Activator.CreateInstance( configType ) as IEntityConfiguration
+             ?? throw new NullReferenceException(
+                    $"Could not create an instance of entity configurator type {configType.Name}" );
+
+            configurator.Configure( modelBuilder );
+
+            // get the entity type being configured
+            if( (!configType.BaseType?.IsGenericType ?? true ) || !configType.BaseType.IsGenericType)
+                throw new ArgumentException(
+                    $"Type {configType.Name} does not have a generic base type" );
+
+            var entityConfigType = configType.BaseType.GetGenericTypeDefinition();
+            if( entityConfigType != typeof( EntityConfigurator<> ) )
+                throw new ArgumentException(
+                    $"Type {configType.Name} is not derived from {typeof(EntityConfigurator<> )}");
+
+            var genArgs = configType.BaseType.GetGenericArguments();
+            if( genArgs.Length != 1 )
+                throw new ArgumentException(
+                    $"Type {configType.Name} does not have a single generic argument");
+
+            unconfigured.Remove( genArgs[ 0 ] );
+        }
+
+        return unconfigured.Count == 0;
     }
 
     public static void ConfigureEntities<TDbContext>(
